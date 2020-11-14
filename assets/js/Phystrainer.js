12dbf99da5b499ed6c336933fe781a81a00982f7
Ricardo Skewes Zorrilla <ricardoskeweszorrilla@MacBook-Pro-de-Ricardo-6.local>
2020-11-13 19:58:05 -0600

    implementarFuncionesPRO

--- assets/js/Phystrainer.js
+++ assets/js/Phystrainer.js
@@ -1,7 +1,124 @@
+const aleatorioEntre = (min, max) => Math.random() * (max - min) + min;
+
+// DISTRIBUCIÓN NORMAL USANDO LA TRANSFORMACIÓN DE BOX-MULLER.
+
+function randn_bm(mu, delta) {
+    var u = 0, v = 0, sigma = delta*mu;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    return sigma*Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )+mu;
+    
+}
+
+//Unidades fundamentales: [m, g, s, A, K, cd, mol] (NOTA: La Unidad Fundamental de masa en el SI es kg, pero se tomará como g para simplificar el código)
+var magnitudes = {
+    masa: [-4,5, "m", [0,1,0,0,0,0,0]],
+    densidad: [-3, 5, "rho" ,[-3,1,0,0,0,0,0]],
+    volumen: [-9, 1, "V" ,[3,0,0,0,0,0,0]],
+    area: [-6, 1, "A" ,[2,0,0,0,0,0,0]],
+    longitud: [-4,1, "l" ,[1,0,0,0,0,0,0]],
+    velocidad: [-3, 2, "v" ,[1,0,-1,0,0,0,0]],
+    tiempo: [-2, 3, "t", [0,0,1,0,0,0,0]],
+    aceleracion: [-2, 1.2, "a", [1,0,-2,0,0,0,0]],
+    fuerza: [-1, 7, "F", [1,1,-2,0,0,0,0]],
+    velAngular: [-2, 1, "omega", [0,0,-1,0,0,0,0]],
+    voltaje: [-1, 5, "V", [2, 1, -3, -1, 0, 0, 0]],
+    corriente: [-5, -1, "I", [0,0,0,1,0,0,0]],
+    temperatura:[2.38, 2.77, "T", [0,0,0,0,1,0,0]],
+    torque: [0, 9, "tau", [2,1,-2,0,0,0,0]], 
+    resistencia: [-1, 10, "R", [2, 1, -3, -2]],
+    coefFriccion: [-2, 0.05, "mu", [0,0,0,0,0,0,0]]
+}
+
+
+function hallarRangoLog(expMin, expMax) {
+    do {
+        lim1 = aleatorioEntre(expMin, expMax);
+        lim2 = aleatorioEntre(expMin, expMax);
+    } while (Math.abs(lim1-lim2)>2);
+
+    return [lim1, lim2]
+}
+
+
+//y = Ax + B
+function generarParamLineal(limX, limY) {
+    A_lim = Math.abs((Math.pow(10,limY[1])-Math.pow(10,limX[0]))/(Math.pow(10,limX[1])-Math.pow(10,limY[0])))
+    //Pendiente
+    A = aleatorioEntre(-A_lim, A_lim);
+    //Punto Prueba
+    x0 = aleatorioEntre(Math.pow(10,limX[0]), Math.pow(10,limX[1]));
+    y0 = aleatorioEntre(Math.pow(10,limY[0]), Math.pow(10,limY[1]));
+    //Ordenada
+    B = y0-A*x0;
+    return [A, B]
+}
+
+// y = Ae^(Bx)
+function generarParamExponencial(limX,limY) {
+    P_log_lim = Math.abs((limY[1]-limY[0])/(Math.pow(10,limX[1])-Math.pow(10,limX[0])));
+    //Pendiente
+    P_log = aleatorioEntre(-P_log_lim, P_log_lim);
+    //Punto Prueba
+    x0 = aleatorioEntre(Math.pow(10, limX[0]), Math.pow(10,limX[1]));
+    y0 = aleatorioEntre(limY[0], limY[1]);
+    //Ordenada 
+    O_log = y0-P_log*x0
+    
+    A = Math.pow(10, O_log)
+    B = P_log/Math.log10(Math.E)
+
+    return [A, B]
+}
+
+// y = Ax^B
+function generarParamPotencial(limX,limY) {
+    B_lim = Math.abs((limY[1]-limY[0])/(limX[1]-limX[0]));
+    //Pendiente
+    B = aleatorioEntre(-B_lim, B_lim);
+    //Punto Prueba
+    x0 = aleatorioEntre(limX[0], limX[1]);
+    y0 = aleatorioEntre(limY[0], limY[1]);
+    //Ordenada 
+    O_log = y0-B*x0
+    
+    A = Math.pow(10, O_log)
+
+     return [A, B]
+}
+
+function generarFuncion(){
+    switch(this.relacion){
+        case 'lineal':
+            this.f = (x) => this.a*x + this.b;
+            break;
+        case 'potencial':
+            this.f = (x) => this.a*Math.pow(x, this.b);
+            break;
+        case 'exponencial':
+            this.f = (x) => this.a*Math.exp(-this.b*x);
+            break;
+        case 'sinoidal':
+            this.f = (x) => this.a*Math.cos(this.b*x - this.c);
+            break;
+        default:
+            this.f = (x) => 0;
+            break;
+    }
+    return this.f;
+}
+
+
+
+
+
+
+
+
 
 
 //Creo que esto lo vamos a tener que cambiar... :(
-const aleatorioEntre = (min, max) => Math.random() * (max - min) + min;
+/*  
 class PhysTrainerProblem{
     constructor(relacion, o={}){
         this.relacion = relacion;
@@ -63,5 +180,5 @@ class PhysTrainerProblem{
         }
     }
 }
-
+*/
 
