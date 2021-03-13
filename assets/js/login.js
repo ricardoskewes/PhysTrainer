6f6130f6d46a8a1d1b03fa3c96c7f6f527131dd6
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-03-13 03:05:07 -0600

    Hice el login pq tenia insomnio xd

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,36 +1,3 @@
-// Login here
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyAt21y6iMy45pQ814LMy93hlYmqSKCn4Lo",
-    authDomain: "physolympiadtrainer.firebaseapp.com",
-    databaseURL: "https://physolympiadtrainer.firebaseio.com",
-    projectId: "physolympiadtrainer",
-    storageBucket: "physolympiadtrainer.appspot.com",
-    messagingSenderId: "205173611989",
-    appId: "1:205173611989:web:55c0ccbc684dfd4300b15c",
-    measurementId: "G-FRR34SGL67"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
-
-
-
-// Al dar submit al login
-    
-document.querySelector('form').addEventListener('submit', async e=>{
-    e.preventDefault();
-    let email = document.querySelector("#email").value;
-    let password = document.querySelector("#password").value;
-    try{
-        let user = await firebase.auth().signInWithEmailAndPassword(email, password);
-        alert("Inicio de sesion")
-    } catch(e){
-        alert(e.message);
-        console.log(e.code);
-    }
-})
 
 
