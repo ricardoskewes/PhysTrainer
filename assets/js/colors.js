6ea88685e7aff59357b2c71a1f7270637a64ea86
Ricardo Skewes <rskeweszorrilla@gmail.com>
2021-04-04 17:06:34 -0500

    Co-authored-by: Eduardo Villalpando Mello <edvilme@users.noreply.github.com>

--- assets/js/colors.js
+++ assets/js/colors.js
@@ -1,50 +1,71 @@
 //Array colores disponibles
-const colores = [
-    "#6EE744",
-    "#D0E744",
-    "#E744A6",
-    "#44E7C0",
-    "#44E78F", 
-    "#44C9E7",
-    "#8244E7",
-    "#E7446B",
-];
 
-const colPrim = colores[Math.floor(Math.random()*colores.length)];
-let root = document.documentElement;
-root.style.setProperty('--main_color', colPrim);
-var coloresUtilizados = [colPrim];
 
-for (let i = 1; i < 6; i++) {
-    let str = "--secondary_color-"+i;
-    root.style.setProperty(str, seleccionarColor());
-}
-
-console.log(root.style.getPropertyValue("--secondary_color-1"))
-
-function seleccionarColor() {
-    let color = colores[Math.floor(Math.random()*colores.length)];
-    if(coloresUtilizados.includes(color)){
-        return seleccionarColor();
-    } else{
-        coloresUtilizados.push(color);
-        return color;
+class Colors {
+    
+    constructor(){
+        this.colPrim = Colors.colores[Math.floor(Math.random()*Colors.colores.length)];
+        this.coloresUtilizados = [this.colPrim];
+        this.root.style.setProperty('--main_color', this.colPrim);
+        
+        for (let i = 1; i <= this.numberOfSecondaryColors; i++) {
+            let str = "--secondary_color-"+i;
+            this.root.style.setProperty(str, this.seleccionarColor());
+        }
     }
-}
+    static colores = [
+        "#6EE744",
+        "#D0E744",
+        "#E744A6",
+        "#44E7C0",
+        "#44E78F", 
+        "#44C9E7",
+        "#8244E7",
+        "#E7446B",
+    ];
 
+    root = document.documentElement;
+    numberOfSecondaryColors = 5;
 
-
-
-
-
-
-//Efecto scroll
-
-window.onscroll = (e)=>{
-    console.log(document.documentElement.scrollTop)
-    if(document.body.scrollTop > window.innerHeight/5 || document.documentElement.scrollTop > window.innerHeight/5){
-        document.querySelector('nav').classList.add('scroll-collapse');
-    } else {
-        document.querySelector('nav').classList.remove('scroll-collapse');
+    seleccionarColor() {
+        let color = Colors.colores[Math.floor(Math.random()*Colors.colores.length)];
+        if(this.coloresUtilizados.includes(color)){
+            return this.seleccionarColor();
+        } else{
+            this.coloresUtilizados.push(color);
+            return color;
+        }
     }
+
+    generateBlobsInDiv(div, times){
+        for(let i = 0; i<times; i++){
+            var r = Math.floor(Math.random()*this.numberOfSecondaryColors)+1;
+            const svgString = blobs2.svg(
+                {
+                    seed: Math.random(),
+                    extraPoints: Math.floor(Math.random()*5)+3,
+                    randomness: Math.floor(Math.random()*5)+1,
+                    size: Math.random()*300 + 100,
+                },
+                {
+                    fill: `var(--secondary_color-${r})`,
+                },
+            );
+            
+            div.innerHTML+=svgString;
+        }
+    }
+
+    
 }
+
+
+
+
+
+
+
+
+
+
+
