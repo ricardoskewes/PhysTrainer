339c8a9e26f26eb059cf89c820248ec1e51c4c6a
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-11-15 02:43:41 -0600

    Added navbar

---
+++ public/components/PTNavbar/PTNavBar.js
@@ -0,0 +1,25 @@
+// Directory path to this script
+const __dirname = import.meta.url.split('/').slice(0, -1).join('/');
+
+const template = document.createElement('template');
+template.innerHTML = `
+    <link rel="stylesheet" href="${__dirname}/PTNavbar.css">
+    <nav>
+        Phystrainer
+    </nav>
+`
+
+class PTNavbarElement extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+    }
+    connectedCallback(){
+        this.shadowRoot.append(template.content.cloneNode(true));
+    }
+    render(){ 
+
+    }
+}
+
+customElements.define('pt-navbar', PTNavbarElement);
